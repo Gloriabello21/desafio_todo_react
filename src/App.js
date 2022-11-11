8ca1c5e3e4b707b26b31b2c9a325208c67fe7ae8
gloria bello <calderonscarlette69@gmail.com>
2022-11-11 14:23:40 -0300

    resolvidesafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Todo from './components/Todo';
 
 function App() {
+  const defaultState = [
+    { label: "comprar mantequilla" },
+    { label: "comprar pan" },
+    { label: "pagar la luz" },
+    { label: "comprar ropa" },
+    { label: "pagar el agua" }
+  ]
+
+  const [items, setItems] = useState(defaultState)
+  const [newItem, setNewItem] = useState('')
+
+
+
+  const deleteFn = (label) => {
+    setItems(items.filter(item => item.label !== label))
+  }
+  const deleteNm = () => {
+    setItems ([])
+  }
+
+  const list = items.map(item => {
+    return <Todo 
+      onClickfn={() => deleteFn(item.label)}>                                                         
+      {item.label}
+    </Todo>
+  })
+  const onClickfn = () => {
+    setItems([...items, { label: newItem }])
+  }
+    const onChangeFn = (event) => {
+      setNewItem(event.target.value)
+    }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mi lista de cosas por hacer</h1>
+
+      <input
+        type="text"
+        value={newItem}
+        onChange={onChangeFn}
+      />
+      <button onClick={onClickfn} >Agregar</button>
+
+      {list}
+
+    <h2>Total de cosas en la Lista</h2>
+
+    <br />
+            
+            
+    <button onClick={deleteNm}>eliminar todo</button>
+
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
